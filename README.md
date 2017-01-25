1706480ce4e5c25b8fe55db235d6927552100246
Kiran SJ <ksj@nvidia.com>
2017-01-25 11:33:59 +0530

    Implemented module support

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-## Features
+#cuda-driver-development 
+This extension loads the required environment for choosen module into vscode for building and debugging.
 
-This extensions helps using visual studio code for building and debugging cuda driver 
-for mobile platforms
+## Features
+This extension setups the task, launch, setting and cpp/c properties requried for building and debugging cuda driver on target.
+
 
 ## Requirements
 
-You should have P4ROOT and TOP environement variable set before launching code.
+You should have environement variable set before launching code.
+- `P4ROOT` : $P4ROOT/sw/pvt/ksj/vscode/... files are read by this extension and also required for build.
+- `TOP`    : This is required to setup build.
+- `TARGET_IP`: If you need remote debug. If not set this can be changed in .vscode/launch.json file
 
-The following path needs to be synced
+The following packages needs to be installed on host for debugging
+- `gdb-multiarch` : This package can be installed by running command `sudo apt-get install gdb-multiarch`
 
-p4 sync $P4ROOT/sw/pvt/ksj/vscode/...
+The following packages needs to be installed on target for debugging
+- `gdbserver` : This package can be installed by running command `sudo apt-get install gdbserver`.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+Run the following commands
 
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+- `cuda Init`:  Shows a dropdown list of Environment available to choose from. Choosing any one will set the environment for that.
 
 ## Known Issues
 
--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,10 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import { window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument } from 'vscode';
+import { deserialize } from "serializer.ts/Serializer";
 
 var statusBarText = window.createStatusBarItem(StatusBarAlignment.Left);
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -18,67 +20,118 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.EmbInit', () => {
-        Embedded_Linux_Init();        
+    let disposable = vscode.commands.registerCommand('extension.cudaInit', () => {
+        cudaInit();
     });
 
     context.subscriptions.push(disposable);
 }
 
-function Embedded_Linux_Init()  {
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
 
-    if (!process.env.P4ROOT) {        
-        vscode.window.showErrorMessage('P4ROOT environement variable not found');
+
+function cudaInit() {
+    // Check for the env variables
+    if (!process.env.P4ROOT || !process.env.TOP) {
+        vscode.window.showErrorMessage('P4ROOT or TOP environement variable not defined');
         return;
     }
+    console.log("P4ROOT=" + process.env.P4ROOT + " TOP=" + process.env.TOP);
 
-    if (!process.env.TOP) {
-        vscode.window.showErrorMessage('TOP environement variable not found');
-        return;
-    }
-
+    // Get the vscode path from $P4ROOT/sw/pvt/ksj/vscode
     const sourceVscodeFolder = path.join(process.env.P4ROOT, "sw", "pvt", "ksj", "vscode");
-    if(!fs.existsSync(sourceVscodeFolder)) {
+    if (!fs.existsSync(sourceVscodeFolder)) {
         vscode.window.showErrorMessage("P4 path $P4ROOT/sw/pvt/ksj/vscode not synced");
         return;
     }
 
-    console.log("P4ROOT=" + process.env.P4ROOT + " TOP=" + process.env.TOP);
+    // Read the modules.json file from p4 path
+    const modulesFilePath = path.join(sourceVscodeFolder, "modules.json");
+    if (!fs.existsSync(modulesFilePath)) {
+        vscode.window.showErrorMessage('modules file not found in ' + modulesFilePath);
+        return;
+    }
+    let modules = loadModules(modulesFilePath);
 
-    // Get the path of .vscode folder
+    // Get the mames of all the modules
+    let moduleList = modules.map(function (modules: Modules) { return modules.name });
+    console.log("list of modules: " + moduleList.toString());
+
+    // Show a drop down menu of all the modules. The chosen module 
+    // will be initialized
+    vscode.window.showQuickPick(moduleList).then(val => setEnvironment(modules, val));
+}
+
+class Modules {
+    name: string;
+    c_cpp_properties: string;
+    launch: string;
+    tasks: string;
+    settings: string;
+}
+
+function loadModules(file: string): Modules[] {
+    var jsonData = JSON.parse(fs.readFileSync(file).toString());
+    let modules = deserialize<Modules[]>(Modules, jsonData);
+
+    //let module = modules.find(x => x.name === "Embedded_Linux_Cuda");
+    /*for(let module of modules) 
+    {
+        console.log(">name : " + module.name);
+        console.log("c_cpp_properties : " + module.c_cpp_properties);
+        console.log("launch : " + module.launch);
+        console.log("tasks : " + module.tasks);
+        console.log("settings : " + module.settings);
+    }*/
+
+    return modules;
+}
+
+function copyFile(destFile: string, sourceFile: string) {
+    fs.writeFileSync(destFile, fs.readFileSync(sourceFile));
+    return;
+}
+
+function setEnvironment(modules: Modules[], moduleName: string) {
+    let module = modules.find(x => x.name === moduleName);
+
+    // Get the path of .vscode folder in repo and p4 vscode folder
+    const sourceVscodeFolder = path.join(process.env.P4ROOT, "sw", "pvt", "ksj", "vscode");
     const vscodePath = path.join(vscode.workspace.rootPath, ".vscode");
-    // If the .vscode folder does not exist then 
-    // lets create it.
+
+    // If the .vscode folder does not exist then  lets create it.
     if(!fs.existsSync(vscodePath)) {
         fs.mkdirSync(vscodePath);
     }
 
-    // Get the path of the task file
+    // Copy the task file from p4 to .vscode
     const taskFilePath = path.join(vscode.workspace.rootPath, '.vscode', 'tasks.json');
-    const sourceTaskFile = path.join(sourceVscodeFolder, "tasks.json");
-    fs.writeFileSync(taskFilePath, fs.readFileSync(sourceTaskFile));
+    const sourceTaskFile = path.join(sourceVscodeFolder, module.tasks);
+    copyFile(taskFilePath, sourceTaskFile); 
 
     const LaunchFilePath = path.join(vscode.workspace.rootPath, '.vscode', 'launch.json');
-    const sourceLaunchFile = path.join(sourceVscodeFolder, "launch.json");
-    fs.writeFileSync(LaunchFilePath, fs.readFileSync(sourceLaunchFile));
+    const sourceLaunchFile = path.join(sourceVscodeFolder, module.launch);
+    copyFile(LaunchFilePath, sourceLaunchFile);
 
     const cppPropertiesFilePath = path.join(vscode.workspace.rootPath, '.vscode', 'c_cpp_properties.json');
-    const cppPropertiesLaunchFile = path.join(sourceVscodeFolder, "c_cpp_properties.json");
-    fs.writeFileSync(cppPropertiesFilePath, fs.readFileSync(cppPropertiesLaunchFile));
+    const sourcecppPropertiesFile = path.join(sourceVscodeFolder, module.c_cpp_properties);
+    copyFile(cppPropertiesFilePath, sourcecppPropertiesFile);
 
     const settingsFilePath = path.join(vscode.workspace.rootPath, '.vscode', 'settings.json');
-    const settingsLaunchFile = path.join(sourceVscodeFolder, "settings.json");
-    fs.writeFileSync(settingsFilePath, fs.readFileSync(settingsLaunchFile));
+    const sourcesettingsFile = path.join(sourceVscodeFolder, module.settings);
+    copyFile(settingsFilePath, sourcesettingsFile);
 
     // Display a message box to the user
-    vscode.window.showInformationMessage('Embedded Linux init!');
+    vscode.window.showInformationMessage('Initialzed for ' + module.name);
 
     // Indicate in the status bar what are we building.
-    statusBarText.text = "Embedded_Linux";
+    statusBarText.text = module.name;
     statusBarText.show();
     statusBarText.color = "White";
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
+    statusBarText.command = 'workbench.action.tasks.runTask';
+    statusBarText.tooltip = "TOP = " + process.env.TOP;
+    //vscode.commands.executeCommand("workbench.action.tasks.runTask");
+    return;
 }
