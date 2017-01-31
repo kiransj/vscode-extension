10bc9b8ed380b7ed60b93632d889850cdfe88e60
Kiran SJ <ksj@nvidia.com>
2017-01-31 12:14:56 +0530

    implemented cuda settings file

--- src/extension.ts
+++ src/extension.ts
@@ -8,29 +8,90 @@ import { window, commands, Disposable, ExtensionContext, StatusBarAlignment, Sta
 import { deserialize } from "serializer.ts/Serializer";
 
 var statusBarText = window.createStatusBarItem(StatusBarAlignment.Left);
+const cudaSettingsPath = path.join(vscode.workspace.rootPath, ".vscode", "cuda.json");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
+// This function gets called when vscode starts up
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+    // log to console the extension is getting initialized
     console.log('Congratulations, your extension "cuda-driver-development" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.cudaInit', () => {
-        cudaInit();
-    });
+    // Register the commands
+    vscode.commands.registerCommand('extension.cudaInit', () => { cudaInit(); });
+    vscode.commands.registerCommand('extension.cudaSetDebug', () => { cudaSetBuildType("debug"); });
+    vscode.commands.registerCommand('extension.cudaSetRelease', () => { cudaSetBuildType("release"); });
+    vscode.commands.registerCommand('extension.cudaSetInternal', () => { cudaSetBuildConf("internal"); });
+    vscode.commands.registerCommand('extension.cudaSetExternal', () => { cudaSetBuildConf("external"); });
 
-    context.subscriptions.push(disposable);
+    try {
+        var stats= fs.statSync(cudaSettingsPath);        
+        var settings = new cudaSettings();
+        settings.loadSettings(cudaSettingsPath);
+        SetStatusBarInfo(settings);
+        // Check for the env variables
+        if (!process.env.P4ROOT || !process.env.TOP) {
+            vscode.window.showErrorMessage('P4ROOT or TOP environement variable not defined. Some features might not work.');
+            return;
+        }
+    }
+    catch(e) {
+
+    }    
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+
 }
 
+class cudaSettings
+{
+    TOP: string;
+    P4ROOT: string;
+    build_conf: string; //internal or external
+    build_type: string; //debug or release
+    target_product: string; //t186ref, t210ref
+    arch: string; //aarch64 or ARMv7
+    //embedded-linux, l4t, QNX etc
+    build_name: string; 
+
+    cudaSettings()
+    {
+        
+    }
+
+    saveSettings(filename: string)
+    {        
+        var settingsJson = JSON.stringify(this);
+        fs.writeFileSync(filename, settingsJson);
+    }
+
+    loadSettings(filename: string)
+    {             
+        var jsonData = JSON.parse(fs.readFileSync(filename).toString());        
+        // Load the obj and return it.
+        var obj = deserialize<cudaSettings>(cudaSettings, jsonData);        
+        this.TOP = obj.TOP;
+        this.P4ROOT = obj.P4ROOT;
+        this.build_conf = obj.build_conf;
+        this.build_type = obj.build_type;
+        this.target_product = obj.target_product;
+        this.arch = obj.arch;
+        this.build_name = obj.build_name;
+    }
+}
+
+class Modules {
+    description: string;
+    name: string;
+    c_cpp_properties: string;
+    launch: string;
+    tasks: string;
+    settings: string;
+    arch: string;
+    target_product: string;
+}
 
 function cudaInit() {
     // Check for the env variables
@@ -56,7 +117,7 @@ function cudaInit() {
     let modules = loadModules(modulesFilePath);
 
     // Get the mames of all the modules
-    let moduleList = modules.map(function (modules: Modules) { return modules.name });
+    let moduleList = modules.map(function (modules: Modules) { return modules.description });
     console.log("list of modules: " + moduleList.toString());
 
     // Show a drop down menu of all the modules. The chosen module 
@@ -64,38 +125,14 @@ function cudaInit() {
     vscode.window.showQuickPick(moduleList).then(val => setEnvironment(modules, val));
 }
 
-class Modules {
-    name: string;
-    c_cpp_properties: string;
-    launch: string;
-    tasks: string;
-    settings: string;
-}
-
 function loadModules(file: string): Modules[] {
     var jsonData = JSON.parse(fs.readFileSync(file).toString());
     let modules = deserialize<Modules[]>(Modules, jsonData);
-
-    //let module = modules.find(x => x.name === "Embedded_Linux_Cuda");
-    /*for(let module of modules) 
-    {
-        console.log(">name : " + module.name);
-        console.log("c_cpp_properties : " + module.c_cpp_properties);
-        console.log("launch : " + module.launch);
-        console.log("tasks : " + module.tasks);
-        console.log("settings : " + module.settings);
-    }*/
-
     return modules;
 }
 
-function copyFile(destFile: string, sourceFile: string) {
-    fs.writeFileSync(destFile, fs.readFileSync(sourceFile));
-    return;
-}
-
 function setEnvironment(modules: Modules[], moduleName: string) {
-    let module = modules.find(x => x.name === moduleName);
+    let module = modules.find(x => x.description === moduleName);
 
     // Get the path of .vscode folder in repo and p4 vscode folder
     const sourceVscodeFolder = path.join(process.env.P4ROOT, "sw", "pvt", "ksj", "vscode");
@@ -125,13 +162,71 @@ function setEnvironment(modules: Modules[], moduleName: string) {
 
     // Display a message box to the user
     vscode.window.showInformationMessage('Initialzed for ' + module.name);
+    
+    //vscode.commands.executeCommand("workbench.action.tasks.runTask");
 
+    // Set the default build parameters
+    var settings = new cudaSettings();
+    settings.arch = module.arch;
+    settings.build_conf = "external";
+    settings.build_type = "debug";
+    settings.P4ROOT = process.env.P4ROOT;
+    settings.TOP = process.env.TOP;
+    settings.target_product = module.target_product;
+    settings.build_name = module.name;
+
+    settings.saveSettings(cudaSettingsPath);
+
+    SetStatusBarInfo(settings);
+    return;
+}
+
+
+function cudaSetBuildType(buildType: string) {
+    if(!fs.existsSync(cudaSettingsPath)) {
+        vscode.window.showErrorMessage("First run 'cuda Init' before setting build type");
+        return;
+    }
+    // Loads the current settings
+    var settings = new cudaSettings();
+    settings.loadSettings(cudaSettingsPath);
+
+    // Change the settings only if necessary
+    if(settings.build_type !== buildType) {
+        settings.build_type = buildType;
+        settings.saveSettings(cudaSettingsPath);
+        SetStatusBarInfo(settings);
+    }
+}
+
+function cudaSetBuildConf(buildConf: string) {
+    if(!fs.existsSync(cudaSettingsPath)) {
+        vscode.window.showErrorMessage("First run 'cuda Init' before setting build type");
+        return;
+    }
+    // Loads the current settings
+    var settings = new cudaSettings();
+    settings.loadSettings(cudaSettingsPath);
+
+    // Change the settings only if necessary
+    if(settings.build_conf !== buildConf) {
+        settings.build_conf = buildConf;
+        settings.saveSettings(cudaSettingsPath);
+        SetStatusBarInfo(settings);
+    }
+}
+
+function SetStatusBarInfo(settings: cudaSettings)
+{
     // Indicate in the status bar what are we building.
-    statusBarText.text = module.name;
-    statusBarText.show();
+    statusBarText.text = settings.build_name + "_" + settings.build_conf + "_" + settings.build_type;    
     statusBarText.color = "White";
     statusBarText.command = 'workbench.action.tasks.runTask';
-    statusBarText.tooltip = "TOP = " + process.env.TOP;
-    //vscode.commands.executeCommand("workbench.action.tasks.runTask");
+    statusBarText.tooltip = "TOP="+settings.TOP;
+    statusBarText.show();
+}
+
+function copyFile(destFile: string, sourceFile: string) {
+    fs.writeFileSync(destFile, fs.readFileSync(sourceFile));
     return;
 }
